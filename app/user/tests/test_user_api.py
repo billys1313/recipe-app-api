d1a0c144f34da7050e3c91a5ef021d63d75ee652
Vasilis Anagnostou <billys70@windowslive.com>
2024-05-08 17:07:41 +0300

    Commit

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -111,7 +111,7 @@ class PublicUserApiTests(TestCase):
 
     def test_retrieve_user_unauthorized(self):
         """Test authentication is required for users."""
-        res = self.client.post(ME_URL)
+        res = self.client.get(ME_URL)
         
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
@@ -121,13 +121,13 @@ class PrivateUserApiTests(TestCase):
         self.user = create_user(
             email='test@example.com',
             password='testpass123',
-            name='Test Name'
+            name='Test Name',
         )
         self.client = APIClient()
-        self.client.force_authentication(user=self.user)
+        self.client.force_authenticate(user=self.user)
     
     def test_retrieve_profile_success(self):
-        """Test retrieving profile for togged in user."""
+        """Test retrieving profile for logged in user."""
         res = self.client.get(ME_URL)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
@@ -138,8 +138,8 @@ class PrivateUserApiTests(TestCase):
 
     def test_post_me_not_allowed(self):
         """Test POST is not allowed for me the endpoint."""
-        res = self.client.get(ME_URL, {})
-        self.assertEqual(res.status_code, status.HTTP_405_NOT_ALLOWED)
+        res = self.client.post(ME_URL, {})
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_update_user_profile(self):
         """Test updating the user profile for the authenticated user."""
@@ -149,5 +149,5 @@ class PrivateUserApiTests(TestCase):
 
         self.user.refresh_from_db()
         self.assertEqual(self.user.name, payload['name'])
-        self.assertTrue(self.user.check_password(payload['name']))
+        self.assertTrue(self.user.check_password(payload['password']))
         self.assertEqual(res.status_code, status.HTTP_200_OK)
